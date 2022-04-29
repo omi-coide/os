b16ac20691b0882caa2111675585541e8690548f
mortis-0 <1190200714@stu.hit.edu.cn>
2022-04-30 02:09:08 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+#[cfg(not(test))]
+
+mod lang_items;
+mod sbi;
+
+#[macro_use]
+mod console;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    //sbi::console_putchar('1' as usize);
+    println!("Hello, Rcore");
+    println!("Shutdown Imminent!");
+    sbi::shutdown();
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0)}
+    });
+}
